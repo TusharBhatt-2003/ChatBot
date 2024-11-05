f148f3859560f21a815c3b6a2c78a764c99f5191
Tushar Bhatt <112729468+TusharBhatt-2003@users.noreply.github.com>
2024-11-06 04:01:09 +0530

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
+import ChatBox from './components/ChatBox';
 
 const App = () => {
 
   return (
     <div className="flex flex-col h-screen justify-center items-center bg-black">
       <p className="text-3xl font-bold text-white mb-4">ChatBot</p>
-     
+     <ChatBox />
     </div>
   );
 };
